7ee3453338312023c6d1ba22eae44a3d4ab2f033
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-06-02 02:15:41 -0500

    Click and Drag Kinda Works
    Sticking to item slots only works for specific values then breaks if different values are used, need to work on universal system.

---
+++ The Way of A Cafe/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    private Vector2 currentPos;
+    public int slotNumber = 0;
+
+    private float x = 0f;
+    private float y = 0f;
+
+    private float x2 = 0f;
+    private float y2 = 0f;
+
+    private float newX, newY;
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null)
+        {
+            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            currentPos = GetComponent<RectTransform>().anchoredPosition;
+            x = currentPos.x; 
+            y = currentPos.y;
+            x2 = currentPos.x;
+            y2 = currentPos.y;
+
+            if(x < 0)
+            {
+                x2 *= -1f;
+                y2 *= -1f;
+            }
+
+            newX = x + x2;
+            newY = y + y2;
+
+            if (newY == 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == slotNumber)
+                    {
+                        newX += (100 * i);
+                    }
+                }
+            }
+            if(newX == 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    if (i == slotNumber)
+                    {
+                        newY += (100 * i);
+                    }
+                }
+            }
+                
+
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = new Vector2(newX, newY);
+        }
+    }
+}

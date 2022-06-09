168d541d32f733cf7c84b0b436b53680a60a51a6
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-06-09 03:28:00 -0500

    NEED TO FIX
    Bug with click and drag system need to set up new checker for nearest slot(Seek code comments in all caps for suggested solution)

--- The Way of A Cafe/Assets/NewAssets/Scripts/ClickDrag.cs
+++ The Way of A Cafe/Assets/NewAssets/Scripts/ClickDrag.cs
@@ -17,7 +17,7 @@ public class ClickDrag : MonoBehaviour
     Slot nearestSlot = null; // create a nearest slot variable that holds the closest slot
     Slot swapSlot = null; // creates a slot variable that holds the slot that swaps items with the closests slot
 
-    private void Update()
+    private void Update() // NEED TO UPDATE - HAVE SLOTS BE ACTIVE IN GAME HIREARCHY AND SET THE AMOUNT AND IMAGE TO INACTIVE CHECK FOR HIREARCHY ACTIVITY(search up how again) THEN IF SLOT IS VISIBLE SET GAMEOBJECT TO ACTIVE IF NOT INACTIVE, SET NEARESTSLOT TO NEAREST ACTIVE SLOT
     {
         if (Input.GetMouseButtonUp(0)) // when mouse button is released
         {
@@ -131,10 +131,96 @@ public class ClickDrag : MonoBehaviour
         return itemTaken;
     }
 
+    /*
     public void OnDrop(Slot slot) // switch drop method to hear and make sure swap slot still works
     {
         Debug.Log("Dropped");
-    }
+        if (currentItem != null) // if the current item exists
+        {
+            RectTransform itemTransform = customCursor.GetComponent<RectTransform>();
+            
+            CanvasGroup canvasGroup = itemTransform.Find("Icon").GetComponent<CanvasGroup>();
+            canvasGroup.alpha = 1f;
+            canvasGroup.blocksRaycasts = true;
+            CanvasGroup canvasGroupA = itemTransform.Find("Amount").GetComponent<CanvasGroup>();
+            canvasGroupA.alpha = 1f;
+            canvasGroupA.blocksRaycasts = true;
+            
+            customCursor.gameObject.SetActive(false); // make customcursor invisible
+
+            RectTransform slotTransform = slot.GetComponent<RectTransform>(); // Create a object refering to the rect transform of the nearest slot 
+            if (swapSlot == null || currentItem == slot.item || slot.item == null) // If there is no item in swap slot or the item held is equal to the nearest slot or the nearest slot has no item
+            {
+                if (slot.item != currentItem) // If the nearest slot item IS NOT equal to the held item
+                {
+                    slot.itemAmount = customCursor.itemAmount; // Sets the nearest slot amount to the amount held in custom cursor
+                }
+                else // If the nearest slot item IS equal to the item held
+                {
+                    slot.itemAmount += customCursor.itemAmount; // Adds the nearest slot amount to the amount held in custom cursor
+                }
+                slot.item = currentItem; // Sets the nearest slot item to held item
+                slot.gameObject.SetActive(true); // set the closest crafting slot to visible
+                whichSlots = slot.gameObject.tag; // Sets which slot to the tag of the nearest slot
+                sendItem = currentItem; // Sets the item sent to the item held
+                sendIndex = slot.index; // sets the index sent to the index of the nearest slot
+                Image image = slotTransform.Find("Icon").GetComponent<Image>(); // Creates an object refering to the image componenet of the "Icon" game object using the rect transform of the parent object to find it
+                image.sprite = currentItem.icon; // set the sprite of the closest crafting slot to the sprite of the current item held
+                TextMeshProUGUI itemText = slotTransform.Find("Amount").GetComponent<TextMeshProUGUI>(); // Creates an object refering to the text component of the "Amount" game object using the rect transform of the parent to find it
+                if (slot.itemAmount == 1) // if the amount of items of the nearest slot is equal to 1
+                {
+                    itemText.SetText(""); // Show no number text since icon iteself can represent 1 item
+                }
+                else // if the amount of items of the nearest slot is more than one
+                {
+                    itemText.SetText(slot.itemAmount.ToString()); // Change text to show the amount of items
+                }
+            }
+            else
+            {
+                RectTransform swapSlotTransform = swapSlot.GetComponent<RectTransform>(); // Get the rect transform of the swap slot 
+                swapSlot.item = slot.item; // sets the item of swapped slot to the item of the nearest slot
+                swapSlot.itemAmount = slot.itemAmount; // sets the amount of the swapped slot to the amount of the nearest slot
+                swapSlot.gameObject.SetActive(true); // set the closest crafting slot to visible
+                whichSlots = swapSlot.gameObject.tag; // sets the which slot to the swap slot tag
+                sendItem = swapSlot.item; // sets the item sent to the item within the swapped slot
+                sendIndex = swapSlot.index; // sets the index sent to the index of the swapped slot
+                Image swapImage = swapSlotTransform.Find("Icon").GetComponent<Image>(); // Creates an object refering to the image component of the "Icon" game object using the rect transform of the parent objet to find it
+                swapImage.sprite = swapSlot.item.icon; // set the sprite of the closest crafting slot to the sprite of the current item held
+                TextMeshProUGUI swapItemText = swapSlotTransform.Find("Amount").GetComponent<TextMeshProUGUI>();// Creates an object refering to the text component of the "Amount" game object using the rect transform of the parent to find it
+                swapItemText.SetText(swapSlot.itemAmount.ToString());
+                swapSlot = null;
+
+                if (slot.item != currentItem)
+                {
+                    slot.itemAmount = customCursor.itemAmount;
+                }
+                else
+                {
+                    slot.itemAmount += customCursor.itemAmount;
+                }
+                slot.item = currentItem;
+                slot.gameObject.SetActive(true); // set the closest crafting slot to visible
+                whichSlots = slot.gameObject.tag; // sets the which slot to the swap slot tag
+                sendItem = currentItem; // sets the item sent to the item held
+                sendIndex = slot.index; // sets the index send to the index of the nearest slot
+                Image image = slotTransform.Find("Icon").GetComponent<Image>();// Creates an object refering to the image component of the "Icon" game object using the rect transform of the parent objet to find it
+                image.sprite = currentItem.icon; // set the sprite of the closest crafting slot to the sprite of the current item held
+                TextMeshProUGUI itemText = slotTransform.Find("Amount").GetComponent<TextMeshProUGUI>();// Creates an object refering to the text component of the "Amount" game object using the rect transform of the parent to find it
+                if (slot.itemAmount == 1) // if the amount is equal to 1
+                {
+                    itemText.SetText(""); // Show no amount text
+                }
+                else // if amount is greater than 1
+                {
+                    itemText.SetText(slot.itemAmount.ToString()); // show amount text
+                }
+            }
+            currentItem = null; // set current item to null;
+        }
+        itemTaken = false; // sets item taken from product to false
+    }*/
+
 
     public void OnMouseDownItem(ItemObject item) //When mouse button is clicked down
     {
@@ -143,6 +229,14 @@ public class ClickDrag : MonoBehaviour
         {
             currentItem = item; // set current item to the item clicked
             customCursor.gameObject.SetActive(true); // set the moveable cursor visible
+            /*
+            CanvasGroup canvasGroup = itemTransform.Find("Icon").GetComponent<CanvasGroup>();
+            canvasGroup.alpha = .6f;
+            canvasGroup.blocksRaycasts = false;
+            CanvasGroup canvasGroupA = itemTransform.Find("Amount").GetComponent<CanvasGroup>();
+            canvasGroupA.alpha = .6f;
+            canvasGroupA.blocksRaycasts = false;
+            */
             Image image = itemTransform.Find("Icon").GetComponent<Image>(); // creates an object reference of the image componenent of the "Icon" game object from the rect transform of custom cursor
             image.sprite = currentItem.icon; // change cursor sprite to item clicked
             customCursor.itemAmount = item.amount; // sets the custom cursor to the item amount 
@@ -166,6 +260,14 @@ public class ClickDrag : MonoBehaviour
             slot.item = null;
             slot.gameObject.SetActive(false);
             customCursor.gameObject.SetActive(true); // set the moveable cursor visible
+            /*
+            CanvasGroup canvasGroup = slotTransform.Find("Icon").GetComponent<CanvasGroup>();
+            canvasGroup.alpha = .6f;
+            canvasGroup.blocksRaycasts = false;
+            CanvasGroup canvasGroupA = slotTransform.Find("Amount").GetComponent<CanvasGroup>();
+            canvasGroupA.alpha = .6f;
+            canvasGroupA.blocksRaycasts = false;
+            */
             Image image = slotTransform.Find("Icon").GetComponent<Image>();
             image.sprite = currentItem.icon; // change cursor sprite to item clicked
             customCursor.itemAmount = slot.itemAmount;
@@ -192,6 +294,14 @@ public class ClickDrag : MonoBehaviour
             slot.item = null;
             slot.gameObject.SetActive(false);
             customCursor.gameObject.SetActive(true); // set the moveable cursor visible
+            /*
+            CanvasGroup canvasGroup = slotTransform.Find("Icon").GetComponent<CanvasGroup>();
+            canvasGroup.alpha = .6f;
+            canvasGroup.blocksRaycasts = false;
+            CanvasGroup canvasGroupA = slotTransform.Find("Amount").GetComponent<CanvasGroup>();
+            canvasGroupA.alpha = .6f;
+            canvasGroupA.blocksRaycasts = false;
+            */
             Image image = slotTransform.Find("Icon").GetComponent<Image>();
             image.sprite = currentItem.icon; // change cursor sprite to item clicked
             customCursor.itemAmount = slot.itemAmount;
